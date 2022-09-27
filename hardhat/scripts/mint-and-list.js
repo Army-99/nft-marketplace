4294a298af6b1531c930b718e1f1a0ea1abedeb5
Army-99 <christian.armato99@gmail.com>
2022-09-27 16:28:36 +0200

    Fixed moralis error (Didn't show events log)

--- hardhat/scripts/mint-and-list.js
+++ hardhat/scripts/mint-and-list.js
@@ -4,11 +4,12 @@ const PRICE = ethers.utils.parseEther("0.1")
 
 const mintAndList = async() => {
     /** ERROR IT DOESN'T NEED TO DEPLOY EVERY TIME */
-    await deployments.fixture(["all"])
+    //await deployments.fixture(["all"])
     const nftMarketplace = await ethers.getContract("NftMarketplace")
+
     const basicNft = await ethers.getContract("BasicNFT")
     
-    console.log("Minting NFT...")
+    console.log(`Minting NFT...`)
     const txMinting = await basicNft.mintNft();
     const txMintingReceipt = await txMinting.wait(1)
     const tokenId = txMintingReceipt.events[0].args.tokenId;
@@ -17,7 +18,7 @@ const mintAndList = async() => {
     const txApprove = await basicNft.approve(nftMarketplace.address, tokenId)
     await txApprove.wait(1)
 
-    console.log("Adding NFT in the marketplace")
+    console.log(`Adding NFT in the marketplace address: ${nftMarketplace.address}` )
     const txAdd = await nftMarketplace.addNft(basicNft.address, PRICE ,tokenId)
     await txAdd.wait(1)
     console.log("Added!")
